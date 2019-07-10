cf01eb64eeb000a6e7ee0899a912f01fc47b3e54
Anthony Valle <valleganthony@gmail.com>
2019-07-10 15:39:26 -0400

    final update

--- src/main/java/com/example/ms3/Data.java
+++ src/main/java/com/example/ms3/Data.java
@@ -8,7 +8,7 @@ import lombok.ToString;
 
 import javax.persistence.*;
 
-@Getter @Setter @NoArgsConstructor @ToString
+
 @Entity
 public class Data {
 
@@ -37,7 +37,96 @@ public class Data {
     @CsvBindByPosition(position = 9)
     private String city;
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNameOne() {
+        return nameOne;
+    }
+
+    public void setNameOne(String nameOne) {
+        this.nameOne = nameOne;
+    }
+
+    public String getNameTwo() {
+        return nameTwo;
+    }
+
+    public void setNameTwo(String nameTwo) {
+        this.nameTwo = nameTwo;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getImageLink() {
+        return imageLink;
+    }
+
+    public void setImageLink(String imageLink) {
+        this.imageLink = imageLink;
+    }
+
+    public String getCardProcessor() {
+        return cardProcessor;
+    }
+
+    public void setCardProcessor(String cardProcessor) {
+        this.cardProcessor = cardProcessor;
+    }
+
+    public String getCashValue() {
+        return cashValue;
+    }
+
+    public void setCashValue(String cashValue) {
+        this.cashValue = cashValue;
+    }
+
+    public String getBooleanOne() {
+        return booleanOne;
+    }
+
+    public void setBooleanOne(String booleanOne) {
+        this.booleanOne = booleanOne;
+    }
+
+    public String getBooleanTwo() {
+        return booleanTwo;
+    }
+
+    public void setBooleanTwo(String booleanTwo) {
+        this.booleanTwo = booleanTwo;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Data() {
+    }
 
 
 }
